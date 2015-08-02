3cc24e8cf5fcd2acd3076d053e33a748fde7b89a
Kageetai <ma@kageetai.net>
2015-08-02 14:04:46 +0200

    working slider and first version of parallax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1.2,
+    centeredSlides: true,
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    paginationBulletRender: function (index, className) {
+      return '<span class="' + className + '"> Nacht des nacherzählten Spiels #' + (index + 1) + '</span>';
+    },
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    spaceBetween: 100
+  })
+});

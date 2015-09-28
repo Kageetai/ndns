ec4b57d50b0728549e507028fe75778b10a051a5
Kageetai <ma@kageetai.net>
2015-09-28 09:49:28 +0200

    fixed nav, header logos and cleanup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,27 @@
 console.log('\'Allo \'Allo!');
 
 $(document).ready(function () {
-  //initialize swiper when document ready
-  var mySwiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: false,
-    slidesPerView: 1.3,
-    centeredSlides: true,
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    paginationBulletRender: function (index, className) {
-      return '<span class="' + className + '"> Nacht des nacherzählten Spiels #' + (index + 1) + '</span>';
-    },
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    spaceBetween: 100,
-    effect: 'coverflow',
-    coverflow: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true
+  // smooth scroll to anchor
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - parseInt(target.css('margin-top'))
+        }, 750);
+        return false;
+      }
+    }
+  });
+
+  // show fixed nav when scrolling
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll >= $(window).height()) {
+      $('nav').addClass('fixed');
+    } else {
+      $('nav').removeClass('fixed');
     }
   });
 });

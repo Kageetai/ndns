52aac9a45ee99d73182208ea4a5f1dc518037c29
Kageetai <ma@kageetai.net>
2015-10-01 14:55:22 +0200

    contact form and send mail per php

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(function () {
   // smooth scroll to anchor
   $('a[href*=#]:not([href=#])').click(function() {
@@ -65,4 +67,82 @@ $(document).ready(function () {
     $(window).resize();
     $('.video-active').text(v.name);
   }
+
+  function hasFormValidation () {
+    return (typeof document.createElement('input').checkValidity === 'function');
+  }
+
+  function addError (el) {
+    return el.parent().addClass('has-error');
+  }
+
+  if (!hasFormValidation()) {
+    $('#contact-form').submit(function () {
+      var hasError = false,
+        name     = $('#form-name'),
+        mail     = $('#form-email'),
+        subject  = $('#form-subject'),
+        message = $('#form-message'),
+        testmail = /^[^0-9][A-z0-9._%+-]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/,
+        $this    = $(this);
+
+      $this.find('div').removeClass('has-error');
+
+      if (name.val() === '') {
+        hasError = true;
+        addError(name);
+      }
+
+      if (!testmail.test(mail.val())) {
+        hasError = true;
+        addError(mail);
+      }
+
+      if (subject.val() === '') {
+        hasError = true;
+        addError(subject);
+      }
+
+      if (message.val() === '') {
+        hasError = true;
+        addError(message);
+      }
+
+      return hasError === false;
+
+    });
+  }
+
+  function sendData() {
+    var XHR = new XMLHttpRequest();
+
+    // We bind the FormData object and the form element
+    var FD  = new FormData(form);
+
+    // We define what will happen if the data are successfully sent
+    XHR.addEventListener("load", function(event) {
+      alert(event.target.responseText);
+    });
+
+    // We define what will happen in case of error
+    XHR.addEventListener("error", function(event) {
+      alert('Oups! Something goes wrong.');
+    });
+
+    // We setup our request
+    XHR.open("POST", "/contact.php");
+
+    // The data sent are the one the user provide in the form
+    XHR.send(FD);
+  }
+
+  // We need to access the form element
+  var form = document.getElementById("contact-form");
+
+  // to takeover its submit event.
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    sendData();
+  });
 });

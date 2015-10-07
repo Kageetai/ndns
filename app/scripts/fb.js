35b00c2f23212cb8492f1107acfafc0c08a8ca1a
Kageetai <ma@kageetai.net>
2015-10-07 10:38:39 +0200

    config.json with sensitive data

--- app/scripts/fb.js
+++ app/scripts/fb.js
@@ -1,52 +1,57 @@
+'use strict';
+
+var NDNS = window.NDNS || {};
+
 $(document).ready(function () {
-  var events = [];
-  var events_future = $('#events-future');
-  var events_past = $('#events-past');
-  var access_token;
+  NDNS.init_fb = function () {
+    var events = [];
+    var events_future = $('#events-future');
+    var events_past = $('#events-past');
+    var access_token;
 
-  $.get('https://graph.facebook.com/v2.4/oauth/access_token?client_id=152804634760737&client_secret=9b87d97b9411a14f9b66bd4837b648ed&grant_type=client_credentials&access_token=CAACKZBZAn5liEBAKTzM6mXDIwgwoigaPOjsZAr9GoO2Qtn7Al2bC1hT6YNTSkIrEiGNgwFxGdB7yEihhV1zZCnQ918J8ZBrIAJrJBsvcvlc1rDLYbKR97w7S0oo8EMyVXWfsT959uZAhHnl5DqvCulKnNK4KZCJOLASbxEVz7c4EBQZA4QmeZA3hm9iB8SZClN6Y1USMRW1iu0qAZDZD', function (response) {
-    access_token = response.access_token;
-  });
-
-  $.ajaxSetup({cache: true});
-  $.getScript('//connect.facebook.net/de_DE/sdk.js', function () {
-    FB.init({
-      appId: '152804634760737',
-      version: 'v2.4' // or v2.0, v2.1, v2.2, v2.3
+    $.get('https://graph.facebook.com/v2.4/oauth/access_token?client_id='+window.config.fb_client_id+'&client_secret='+window.config.fb_client_secret+'&grant_type=client_credentials', function (response) {
+      access_token = response.access_token;
     });
 
-    FB.api(
-      '/gamestormberlin/events',
-      'GET',
-      {
-        access_token: access_token
-      },
-      fb_callback
-    );
-  });
+    $.ajaxSetup({cache: true});
+    $.getScript('//connect.facebook.net/de_DE/sdk.js', function () {
+      FB.init({
+        appId: window.config.fb_app_id,
+        version: 'v2.4' // or v2.0, v2.1, v2.2, v2.3
+      });
 
-  function fb_callback(response) {
-    console.log(response);
-    events = events.concat(response.data);
-    if (response.paging.next) {
-      $.get(response.paging.next, fb_callback);
-    } else {
-      parseEvents(events);
+      FB.api(
+        '/gamestormberlin/events',
+        'GET',
+        {
+          access_token: access_token
+        },
+        fb_callback
+      );
+    });
+
+    function fb_callback(response) {
+      console.log(response);
+      events = events.concat(response.data);
+      if (response.paging.next) {
+        $.get(response.paging.next, fb_callback);
+      } else {
+        parseEvents(events);
+      }
+    }
+
+    function parseEvents(events) {
+      var today = new Date();
+      $.each(events, function (i, e) {
+        var date = new Date(e.start_time);
+        if (e.name.indexOf('Nacht') >= 0 && date < today) {
+          events_past.append("<li><a href='http://www.facebook.com/events/" + e.id + "'>" + date.toLocaleDateString('de-DE') + " - " + e.name + "</a></li>");
+        } else if (e.name.indexOf('Nacht') >= 0 && date >= today) {
+          events_future.append("<li><a href='http://www.facebook.com/events/" + e.id + "'>" + date.toLocaleDateString('de-DE') + " - " + e.name + "</a></li>");
+        }
+      });
+
+      jQuery(window).trigger('resize').trigger('scroll');
     }
   }
-
-  function parseEvents(events) {
-    var today = new Date();
-    $.each(events, function (i, e) {
-      var date = new Date(e.start_time);
-      if (e.name.indexOf('Nacht') >= 0 && date < today) {
-        events_past.append("<li><a href='http://www.facebook.com/events/" + e.id + "'>" + date.toLocaleDateString('de-DE') + " - " + e.name + "</a></li>");
-      } else if (e.name.indexOf('Nacht') >= 0 && date >= today) {
-        events_future.append("<li><a href='http://www.facebook.com/events/" + e.id + "'>" + date.toLocaleDateString('de-DE') + " - " + e.name + "</a></li>");
-      }
-    });
-
-    jQuery(window).trigger('resize').trigger('scroll');
-  }
-})
-;
+});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,148 +1,161 @@
 'use strict';
 
+var NDNS = window.NDNS || {};
+
 $(document).ready(function () {
-  // smooth scroll to anchor
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - parseInt(target.css('margin-top')) - $('nav').outerHeight()
-        }, 750);
-        return false;
+
+  $.getJSON('config.json', function (data) {
+    window.config = data;
+    NDNS.init();
+    NDNS.init_fb();
+  });
+
+  NDNS.init = function () {
+    // smooth scroll to anchor
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - parseInt(target.css('margin-top')) - $('nav').outerHeight()
+          }, 750);
+          return false;
+        }
       }
-    }
-  });
-
-  // show fixed nav when scrolling
-  $(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-    if (scroll >= $(window).height() * 0.9) {
-      $('nav').addClass('fixed');
-    } else {
-      $('nav').removeClass('fixed');
-    }
-  });
-
-  // load videos
-  var videos = [];
-  var video_active = 0;
-  var video = $('#video');
-  var videolist = $('#video-list');
-  $.getJSON( 'videos.json', function( data ) {
-    videos = data;
-
-    // fill video list
-    for(var i = 0; i < videos.length; i++) {
-      $('<li class=\'video-' + i + '\'>' + videos[i].name + '</li>')
-        .click(function(e) {
-          video_active = this.className.split('-')[1];
-          showVideo(videos[video_active]);
-          videolist.find('> li').removeClass('active');
-          this.classList.add('active');
-        }).appendTo(videolist);
-    }
-
-    showVideo(videos[video_active]);
-    videolist.perfectScrollbar().find('> li:first-child').addClass('active');
-  });
-
-  $('.video-right').click(function() {
-    showVideo(videos[++video_active]);
-  });
-
-  $('.video-left').click(function() {
-    showVideo(videos[--video_active]);
-  });
-
-  // resize video list according to video height
-  $(window).resize(function() {
-    videolist.height(video.outerHeight());
-    videolist.perfectScrollbar('update');
-  });
-
-  function showVideo(v) {
-    video.find('> iframe').attr('src', v.url);
-    $(window).resize();
-    $('.video-active').text(v.name);
-  }
-
-  function hasFormValidation () {
-    return (typeof document.createElement('input').checkValidity === 'function');
-  }
-
-  function addError (el) {
-    return el.parent().addClass('has-error');
-  }
-
-  if (!hasFormValidation()) {
-    $('#contact-form').submit(function () {
-      var hasError = false,
-        name     = $('#form-name'),
-        mail     = $('#form-email'),
-        subject  = $('#form-subject'),
-        message = $('#form-message'),
-        testmail = /^[^0-9][A-z0-9._%+-]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/,
-        $this    = $(this);
-
-      $this.find('div').removeClass('has-error');
-
-      if (name.val() === '') {
-        hasError = true;
-        addError(name);
-      }
-
-      if (!testmail.test(mail.val())) {
-        hasError = true;
-        addError(mail);
-      }
-
-      if (subject.val() === '') {
-        hasError = true;
-        addError(subject);
-      }
-
-      if (message.val() === '') {
-        hasError = true;
-        addError(message);
-      }
-
-      return hasError === false;
-
-    });
-  }
-
-  function sendData() {
-    var XHR = new XMLHttpRequest();
-
-    // We bind the FormData object and the form element
-    var FD  = new FormData(form);
-
-    // We define what will happen if the data are successfully sent
-    XHR.addEventListener("load", function(event) {
-      alert(event.target.responseText);
     });
 
-    // We define what will happen in case of error
-    XHR.addEventListener("error", function(event) {
-      alert('Oups! Something goes wrong.');
+    // show fixed nav when scrolling
+    $(window).scroll(function (event) {
+      var scroll = $(window).scrollTop();
+      if (scroll >= $(window).height() * 0.9) {
+        $('nav').addClass('fixed');
+      } else {
+        $('nav').removeClass('fixed');
+      }
     });
 
-    // We setup our request
-    XHR.open("POST", "/contact.php");
+    // load videos
+    var videos = [];
+    var video_active = 0;
+    var video = $('#video');
+    var videolist = $('#video-list');
+    $.getJSON('videos.json', function (data) {
+      videos = data;
 
-    // The data sent are the one the user provide in the form
-    XHR.send(FD);
+      // fill video list
+      for (var i = 0; i < videos.length; i++) {
+        $('<li class=\'video-' + i + '\'>' + videos[i].name + '</li>')
+          .click(function (e) {
+            video_active = this.className.split('-')[1];
+            showVideo(videos[video_active]);
+            videolist.find('> li').removeClass('active');
+            this.classList.add('active');
+          }).appendTo(videolist);
+      }
+
+      showVideo(videos[video_active]);
+      videolist.perfectScrollbar().find('> li:first-child').addClass('active');
+    });
+
+    $('.video-right').click(function () {
+      showVideo(videos[++video_active]);
+    });
+
+    $('.video-left').click(function () {
+      showVideo(videos[--video_active]);
+    });
+
+    // resize video list according to video height
+    $(window).resize(function () {
+      videolist.height(video.outerHeight());
+      videolist.perfectScrollbar('update');
+    });
+
+    function showVideo(v) {
+      video.find('> iframe').attr('src', v.url);
+      $(window).resize();
+      $('.video-active').text(v.name);
+    }
+
+
+    // CONTACT FORM
+    function hasFormValidation() {
+      return (typeof document.createElement('input').checkValidity === 'function');
+    }
+
+    function addError(el) {
+      return el.parent().addClass('has-error');
+    }
+
+    if (!hasFormValidation()) {
+      $('#contact-form').submit(function () {
+        var hasError = false,
+          name = $('#form-name'),
+          mail = $('#form-email'),
+          subject = $('#form-subject'),
+          message = $('#form-message'),
+          testmail = /^[^0-9][A-z0-9._%+-]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/,
+          $this = $(this);
+
+        $this.find('div').removeClass('has-error');
+
+        if (name.val() === '') {
+          hasError = true;
+          addError(name);
+        }
+
+        if (!testmail.test(mail.val())) {
+          hasError = true;
+          addError(mail);
+        }
+
+        if (subject.val() === '') {
+          hasError = true;
+          addError(subject);
+        }
+
+        if (message.val() === '') {
+          hasError = true;
+          addError(message);
+        }
+
+        return hasError === false;
+
+      });
+    }
+
+    function sendData() {
+      var XHR = new XMLHttpRequest();
+
+      // We bind the FormData object and the form element
+      var FD = new FormData(form);
+
+      // We define what will happen if the data are successfully sent
+      XHR.addEventListener("load", function (event) {
+        alert(event.target.responseText);
+      });
+
+      // We define what will happen in case of error
+      XHR.addEventListener("error", function (event) {
+        alert('Oups! Something goes wrong.');
+      });
+
+      // We setup our request
+      XHR.open("POST", "/contact.php");
+
+      // The data sent are the one the user provide in the form
+      XHR.send(FD);
+    }
+
+    // We need to access the form element
+    var form = document.getElementById("contact-form");
+
+    // to takeover its submit event.
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      sendData();
+    });
   }
-
-  // We need to access the form element
-  var form = document.getElementById("contact-form");
-
-  // to takeover its submit event.
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    sendData();
-  });
 });
